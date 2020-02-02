793207f3c3cc7eb20680cd0d6a7059f3dfbaa349
SiqiWang666 <siqi_astrid@gwu.edu>
2020-02-01 20:31:13 -0500

    Updata checksum funcs

---
+++ src/main/Util.java
@@ -0,0 +1,26 @@
+package src.main;
+
+import java.util.zip.CRC32;
+
+class Util {
+    /**
+     * Message MUST end with a trailing '|' character.
+     * @param message
+     * @return checksum
+     */
+    public static String generateChecksum(String message) {
+        CRC32 crc = new CRC32();
+        crc.update(message.getBytes());
+        return String.valueOf(crc.getValue());
+    }
+    /**
+     * Message MUST end with a trailing '|' character.
+     * @param message
+     * @return isValid  is a valid package
+     */
+    public static boolean validChecksum(String message) {
+        String[] decoded = Package.splitPackage(message);
+        String reportedChecksum = generateChecksum(new String(decoded[0] + "|" + decoded[1] + "|" + decoded[2] + "|"));
+        return reportedChecksum.equals(decoded[3]);
+    }
+}
